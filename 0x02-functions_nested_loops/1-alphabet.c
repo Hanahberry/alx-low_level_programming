6546a8925a563393552cde71999a17d17ae2e2b1
Hanahberry <obiageri.ndukwe@gmail.com>
2022-09-13 10:29:30 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include <main.h>
+#include "main.h"
+
+/**
+ *Print_alphabet - print alphabet in lowercase
+ */
+void print_alphabet(void);
+{
+	char lowercase
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+	_putchar(lowercase)
+	_putchar('\n');
+}
