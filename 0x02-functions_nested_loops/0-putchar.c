44f3a9d7f4224d5ececbd942d66e8dee2c4e7b08
Hanahberry <obiageri.ndukwe@gmail.com>
2022-09-13 09:33:22 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *main - Entry point
+ *
+ *Return: 0 nothing, return void
+ */
+
+int main(void)
+
+{
+	_putchat('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+
+	return (0);
+}	
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,23 @@
+#fndef MAIN_H
+#define MAIN_H
+
+/**
+*file: main_h
+*Description: contain all the functions prototype in this project
+*/
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif /* MAIN_H */
